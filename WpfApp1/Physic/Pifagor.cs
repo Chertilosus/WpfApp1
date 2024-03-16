c5148033891ae82ada35d45e962677423aca2c4f
”Maksim <kofuku839@gmail.com>
2024-03-16 22:33:20 +0300

    initial commit

---
+++ WpfApp1/Physic/Pifagor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Math;
+
+
+namespace WpfApp1.Physic
+{
+    public class Pifagor
+    {
+        static double CalculateHypotenuse(int a, int b, int angel) 
+        {
+            double c = Math.Pow(a, 2) + Math.Pow(b, 2) - (2 * b * a) * Math.Cos(angel);
+            return Math.Sqrt(c);
+        }
+    }
+}
